61a0fd6c8d9dd85824818e38b6e5353e256bea72
orderyo <markovmaks895@gmail.com>
2023-05-11 18:54:29 +0300

    исправил регулярное выражение

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,15 @@ public class Main {
     public static void main(String[] args){
         String ip = "0.0.0.0";
         String ip1 = "191.168.0.1";
-        String ip2 = "255.255.9.9";
-        String ip3 = "255.255.9.10";
-        Pattern pattern = Pattern.compile("^(0|[0-1]([0-9]{2})|(2([0-5]{2})))\\.(0|[0-1]([0-9]{2})|(2([0-5]{2})))\\.\\d\\.\\d$");
+        String ip2 = "255.249.9.9";
+        String ip3 = "255.275.9.1";
+        Pattern pattern = Pattern.compile("^((0|[0-1]([0-9]{2})|2[0-4][0-9]|25[0-5])\\.){2}\\d\\.\\d$");
         Matcher matcher = pattern.matcher(ip);
         System.out.println(pattern.matcher(ip).find());
         System.out.println(pattern.matcher(ip1).find());
         System.out.println(pattern.matcher(ip2).find());
         System.out.println(pattern.matcher(ip3).find());
+
 
     }
 }
