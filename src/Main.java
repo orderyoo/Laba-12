a8406f15ab35eb2d41c4d9b93378caf6a99665bd
orderyo <markovmaks895@gmail.com>
2023-05-20 14:26:46 +0300

    исправил регулярное выражение

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args){
-        String ip = "0.0.0.0";
-        String ip1 = "191.168.0.1";
+        String ip = "1.0.0.0";
+        String ip1 = "191.34.199.1";
         String ip2 = "255.249.9.9";
         String ip3 = "255.275.9.1";
-        Pattern pattern = Pattern.compile("^((0|[0-1]([0-9]{2})|2[0-4][0-9]|25[0-5])\\.){2}\\d\\.\\d$");
+        Pattern pattern = Pattern.compile("^(([0-9]|[1-9][0-9]|1([0-9]{2})|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1([0-9]{2})|2[0-4][0-9]|25[0-5])$");
         Matcher matcher = pattern.matcher(ip);
         System.out.println(pattern.matcher(ip).find());
         System.out.println(pattern.matcher(ip1).find());
